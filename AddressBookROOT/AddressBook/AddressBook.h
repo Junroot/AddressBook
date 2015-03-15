9e085086353cd4d9fd51802b1fc06354c5d75827
ROOT <junroot0909@gmail.com>
2015-03-16 01:00:45 +0900

    15031601
    새로 올림

--- AddressBookROOT/AddressBook/AddressBook.h
+++ AddressBookROOT/AddressBook/AddressBook.h
@@ -2,25 +2,17 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class Address
 {
 public:
 	Address();
 	Address(string name, vector<string> phonenum, int CallerID);
 	~Address();
-	const std::string name() const
-	{
-		return _name;
-	}
 
 private:
-<<<<<<< HEAD
 	string name;
 	vector<string> phonenums;
 	int CallerID;
-=======
-	std::string _name;
-	std::string _phonenum;
-	int _callerID;
->>>>>>> origin/master
 };
